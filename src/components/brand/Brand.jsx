afcb87f7a94537a5b725c5c7d3462f92f6ddf199
ScottishSmokedSalmone <qluka@yahoo.com>
2023-03-27 23:38:03 +0400

    done

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import './brand.css';
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} />
+    </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
+);
+
+export default Brand;
+
+{/*import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import './brand.css';
+
+const Brand = () => {
+  return (
+    <div classname='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  )
+}
+
+export default Brand*/}
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Feature from '../../components/feature/Feature';
+import './whatGPT3.css';
+
+const WhatGPT3 = () => (
+  <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    <div className="gpt3__whatgpt3-feature">
+      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+    </div>
+    <div className="gpt3__whatgpt3-heading">
+      <h1>The possibilities are beyond your imagination</h1>
+      <p>Explore the Library</p>
+    </div>
+    <div className="gpt3__whatgpt3-container">
+      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+    </div>
+  </div>
+);
+
+export default WhatGPT3;
+
+{/*import React from 'react';
+import { Feature } from '../../components';
+import './whatGPT3.css';
+
+const WhatGPT3 = () => {
+  return (
+    <div classname="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div classname="gpt3__whatgpt3-feature">
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      </div>
+      <div classname="gpt3__whatsgpt3-heading">
+        <h1 classname="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div classname="gpt3__whatgpt3-container">
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
+        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3;*/}
